7bb899541f0d031b9950d6804daa8f013cff2841
Jeeshu Mittal <jeeshumittal7@gmail.com>
2024-04-12 21:32:08 +0530

    Added: Tailwind build with dark mode capabilities; Removed: Tailwind Play Cdn

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",
+    "./public/js/**/*.js"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
